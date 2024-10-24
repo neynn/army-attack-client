3623c7d99e9ab504b7aec199751d276d19df2754
neynn <nils.lucky@web.de>
2024-10-24 14:49:18 +0200

    Update resourceLoader.js

--- source/resourceLoader.js
+++ source/resourceLoader.js
@@ -5,7 +5,7 @@ ResourceLoader.DEFAULT_AUDIO_TYPE = ".mp3";
 ResourceLoader.FILE_SERVER_ADDRESS = "Army_Attack_Client";
 
 ResourceLoader.getPath = function(directory, source) {
-    const path = `${ResourceLoader.FILE_SERVER_ADDRESS}/${directory}/${source}`;
+    const path = `${directory}/${source}`;
     return path;
 }
 
