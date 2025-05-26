163538afc8665862e54d1e58b6e3d95b6b455e82
neynn <nils.lucky@web.de>
2025-05-26 22:02:07 +0200

    Update component.js

--- source/entity/component.js
+++ source/entity/component.js
@@ -1,20 +1,5 @@
 export const Component = function(instance) {
     this.instance = instance;
-
-    if(typeof instance.prototype.update === "function") {
-        this.type = Component.TYPE.ACTIVE;
-    } else {
-        this.type = Component.TYPE.INACTIVE;
-    }
-}
-
-Component.TYPE = {
-    INACTIVE: 0,
-    ACTIVE: 1
-};
-
-Component.prototype.isActive = function() {
-    return this.type === Component.TYPE.ACTIVE;
 }
 
 Component.prototype.createInstance = function() {
